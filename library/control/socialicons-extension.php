b580c55174fd55aed42bfdde7c73eb0a24f4fe98
Aram Zucker-Scharff <azuckers@gmu.edu>
2011-10-27 00:37:25 -0400

    Extra social icons script added.

--- library/control/socialicons-extension.php
+++ library/control/socialicons-extension.php
@@ -6,7 +6,7 @@
     $$value['id'] = get_option($value['id'], $value['std']);
   }
   
-  if ( $nmwp_social_rss == true ){
+  if ( $nmwp_social_rss == 'true' ){
   
 	?>
 		<a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/library/imgs/socialmediaicons/rss.png" alt="Syndicate this site using RSS" /></a> 
@@ -14,7 +14,7 @@
   
   }
  
-  if ( $nmwp_social_twit == true ){
+  if ( $nmwp_social_twit == 'true' ){
   
 	?>
 		<a href="http://twitter.com/<?php echo $nmwp_social_twit_un; ?>" title="<?php echo $nmwp_social_twit_un; ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/library/imgs/socialmediaicons/twitter.png" alt="<?php echo $nmwp_social_twit_un; ?>" /></a> 
@@ -22,7 +22,7 @@
   
   }
 
-  if ( $nmwp_social_fb == true ){
+  if ( $nmwp_social_fb == 'true' ){
   
 	?>
 		<a href="http://facebook.com/<?php echo $nmwp_social_fb_un; ?>" title="<?php echo $nmwp_social_fb_un; ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/library/imgs/socialmediaicons/facebook.png" alt="<?php echo $nmwp_social_fb_un; ?>" /></a> 
@@ -30,13 +30,36 @@
   
   }
   
-  if ( $nmwp_social_contribute == true ){
+  if ( $nmwp_social_contribute == 'true' ){
   
 	?>
 		<img src="<?php bloginfo('stylesheet_directory'); ?>/library/imgs/socialmediaicons/write.png" alt="Write for Us." /> 
 	<?php
   
-  }  
+  }
+
+	$iconsarray = explode(",",$nmwp_social_addl_icons);
+	$iconscount = count($iconsarray);
+	$urlsarray = explode(",",$nmwp_social_addl_urls);
+	$urlcount = count($urlsarray); 
+		$k=0;
   
+  if ( $nmwp_social_addl_icons !== "" ){
+  
+
+	
+	foreach ($urlsarray as $key => $value) {
+  
+
+  
+	?>
+		<a href="<?php echo $urlsarray[$key]; ?>"><img src="<?php echo $iconsarray[$key]; ?>" /></a> 
+	<?php
+	
+		$k++;
+	
+	}
+  
+  }  
   
 ?>
