b3863d5d4e7a7c3c9787e99d8295c1096e1cc0c9
Aram Zucker-Scharff <azuckers@gmu.edu>
2011-11-27 17:14:32 -0500

    Added better indicaters for comment link on asides.

--- library/extensions/sidebartop-extensions.php
+++ library/extensions/sidebartop-extensions.php
@@ -13,6 +13,17 @@ function nmwp_widgets_sidebar_init() {
 		'after_title' => '',
 	) );	
 
+	if ( function_exists('register_sidebar') )
+	register_sidebar( array(
+		'name' => __( 'Ad Sidebar Mid', 'thematic' ),
+		'id' => 'ad-sidebar-mid',
+		'description' => __( 'The ad space at the middle of the sidebar, do not use title. 300x250.', 'thematic' ),
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '',
+		'after_title' => '',
+	) );	
+	
 }
 
 add_action( 'widgets_init', 'nmwp_widgets_sidebar_init' );
@@ -20,7 +31,7 @@ add_action( 'widgets_init', 'nmwp_widgets_sidebar_init' );
 // Thumbs and custom field
 function sidebaradtop() {
 ?>
-<div id="sidebartop-ad" class="aside main-aside">
+<div id="sidebar-top-ad" class="aside main-aside">
 					<?php if ( !function_exists('dynamic_sidebar')
 					|| !dynamic_sidebar('Ad Sidebar Top') ) : ?>
 						<img src="<?php bloginfo('stylesheet_directory'); ?>/library/imgs/300x250TestAd.png" />
@@ -29,5 +40,17 @@ function sidebaradtop() {
 <?php
 }
 add_action('thematic_abovemainasides', 'sidebaradtop');
+
+function sidebaradmid() {
+?>
+<div id="sidebar-mid-ad" class="aside main-aside">
+					<?php if ( !function_exists('dynamic_sidebar')
+					|| !dynamic_sidebar('Ad Sidebar Mid') ) : ?>
+						<img src="<?php bloginfo('stylesheet_directory'); ?>/library/imgs/300x250TestAd.png" />
+					<?php endif; ?>  
+</div><!-- end Ad block -->
+<?php
+}
+add_action('thematic_betweenmainasides', 'sidebaradmid');
 
 ?>
